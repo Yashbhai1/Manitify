452a326f7819df14b7d9c0c4f4d483ec8b1483d6
Yash Agarwal <94207669+Yashbhai1@users.noreply.github.com>
2026-03-03 09:30:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+let counter = 0;
+const counterElement = document.getElementById("downloadCounter");
+
+// speed control (higher = slower)
+const intervalTime = 120; // milliseconds
+
+setInterval(() => {
+  counter += Math.floor(Math.random() * 3) + 1; // random 1–3 increase
+  counterElement.innerText = counter.toLocaleString() + "+";
+}, intervalTime);
+
+// OUR SERVICES SECTION //
+const buttons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".service-card");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    buttons.forEach(b => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    const filter = btn.dataset.filter;
+
+    cards.forEach(card => {
+      if (filter === "all" || card.classList.contains(filter)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+});
