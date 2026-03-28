fc95a30746c166d5c59db07432ed736b595ae205
yashbhai1 <yashagarwal5mar2007@gmail.com>
2026-03-28 04:55:01 -0700

    11

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.getElementById("nav-links");
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
 
-menuToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
 });
 
 //  files downloaded counter //
